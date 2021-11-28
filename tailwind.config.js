d075d3e5475e4f230ed00f8f6205c67f0faec376
76tebe <tubagusghiffarimaulana@gmail.com>
2021-11-29 06:38:56 +0700

    add dark mode to each page and add image for auth page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,24 @@ module.exports = {
     extend: {
       colors: {
         'background': '#283149',
-        'content-background-gradient': 'linear-gradient(#283149, #232A3F)',
         'content-background-light': '#F6F6F6',
         'background-component' : '#404B69',
         'background-component-2' : '#333C55',
         'background-component-light': 'rgba(0, 156, 167, 0.16)',
+        'background-component-2-light': 'rgba(0, 156, 167, 0.08)',
         'primary-white' : '#fff',
         'primary-black' : '#292929',
         'subtext' : '#9AADB4',
         'component': '#009CA7',
         'hover-component' : '#2EADB6',
-      }
+      },
+      backgroundImage: {
+        'content-background-gradient': 'linear-gradient(#283149, #232A3F)',
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: { backgroundImage: ['dark'] },
   },
   plugins: [],
 }
